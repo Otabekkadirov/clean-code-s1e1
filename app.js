f1243cd12b3af229bc53e9655999431ae47d25a4
Otabekkadirov <ganijanovich94@gmail.com>
2025-02-02 22:12:50 +0500

    fix(extended-2.1): use BEM for class names fix(basic-3.4): change some tag selectors in CSS and JS fix(basic-3.5): indent in blocks fix(basic-3.6): add space after property name in CSS fix(basic-3.7): add semicolon after property in CSS fix(basic-3.8): separate selectors and properties with line breaks in CSS

--- app.js
+++ app.js
@@ -38,9 +38,9 @@ var createNewTaskElement = function (taskString) {
   editInput.className = "task";
 
   editButton.innerText = "Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className = "edit";
+  editButton.className = "btn btn_edit";
 
-  deleteButton.className = "delete";
+  deleteButton.className = "btn btn_delete";
   deleteButtonImg.src = "./remove.svg";
   deleteButton.appendChild(deleteButtonImg);
 
@@ -76,7 +76,7 @@ var editTask = function () {
 
   var editInput = listItem.querySelector("input[type=text]");
   var label = listItem.querySelector("label");
-  var editBtn = listItem.querySelector(".edit");
+  var editBtn = listItem.querySelector(".btn_edit");
   var containsClass = listItem.classList.contains("editMode");
   //If class of the parent is .editmode
   if (containsClass) {
@@ -138,8 +138,8 @@ var bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
   console.log("bind list item events");
   //select ListItems children
   var checkBox = taskListItem.querySelector("input[type=checkbox]");
-  var editButton = taskListItem.querySelector("button.edit");
-  var deleteButton = taskListItem.querySelector("button.delete");
+  var editButton = taskListItem.querySelector(".btn_edit");
+  var deleteButton = taskListItem.querySelector(".btn_delete");
 
   //Bind editTask to edit button.
   editButton.onclick = editTask;
